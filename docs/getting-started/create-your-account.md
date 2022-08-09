f85e39fff0d472c459ee933c6db0c93c8446d1bd
alexjamesgodfrey <me@alexgodfrey.com>
2022-08-09 18:40:06 -0400

    Init

---
+++ docs/getting-started/create-your-account.md
@@ -0,0 +1,25 @@
+---
+sidebar_position: 1
+---
+
+# Create your account
+
+## Getting Started
+
+Visit the [homepage](https://studyflow.ai)
+and click **Sign up** in the top right or go straight to the [login page](https://www.genome.gov/)
+.
+
+![Homepage](../../static/img/Tutorial/homepage.png)
+
+## Signing in with Google
+
+Our current authentication provider is Google. Using Google helps us keep your information secure. This means that all you need is a Google account to create a Studyflow account! After clicking sign in with Google, select your Google account.
+
+![GoogleSignIn](../../static/img/Tutorial/googlesignin.png)
+
+Don't worry about the weird URL. We have already contacted Google about it. Your're in the right place!
+
+## That's it
+
+Now to the fun part: Setting up your Studyflow profile.
---
+++ docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github")
+const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Studyflow Documentation",
+  tagline: "Learn to learn",
+  url: "https://docs.studyflow.ai",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.svg",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "STUDYFLOWLLC", // Usually your GitHub org/user name.
+  projectName: "studyflow-docs", // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Studyflow Documentation",
+        logo: {
+          alt: "Studyflow Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorials",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://studyflow.ai/dash",
+            label: "Studyflow Dash",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Quick Links",
+            items: [
+              {
+                label: "Dash",
+                href: "https://studyflow.ai/dash",
+              },
+              {
+                label: "Tasks",
+                href: "https://studyflow.ai/tasks",
+              },
+              {
+                label: "Automation",
+                href: "https://studyflow.ai/automation",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Studyflow LLC`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+}
+
+module.exports = config
